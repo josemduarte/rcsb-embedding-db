36c80004a053b0c94659cda781168018a97759db
bioinsilico <jsegurmor@gmail.com>
2024-11-01 11:15:03 -0700

    keep for config && csm option

--- src/db.py
+++ src/db.py
@@ -3,20 +3,25 @@ import chromadb
 import pandas as pd
 
 
-def init_db_collection(chain_path, assembly_path):
+def init_db_collection(chain_path, csm_path, assembly_path):
     chroma_client = chromadb.Client()
     chain_collection = chroma_client.create_collection(
         name="chain_collection",
+        metadata={"hnsw:space": "cosine"}
+    )
+    csm_collection = chroma_client.create_collection(
+        name="csm_collection",
         metadata={"hnsw:space": "cosine"}
     )
     assembly_collection = chroma_client.create_collection(
         name="assembly_collection",
         metadata={"hnsw:space": "cosine"}
     )
-    load_path_to_collection(chain_path, chain_collection)
+    load_path_to_collection(chain_path, chain_collection, csm_collection)
+    load_path_to_collection(csm_path, csm_collection)
     load_path_to_collection(assembly_path, assembly_collection)
 
-    return chain_collection, assembly_collection
+    return chain_collection, csm_collection, assembly_collection
 
 
 def display_progress(current, total):
@@ -26,19 +31,19 @@ def display_progress(current, total):
         print()
 
 
-def load_path_to_collection(embedding_path, db_collection):
-    print(f"DB loading {db_collection.name} path {embedding_path} starts!")
+def load_path_to_collection(embedding_path, *db_collection_list):
+    print(f"DB loading data from path {embedding_path}")
     files = os.listdir(embedding_path)
     for idx, r in enumerate(files):
         instance_id = file_name(r)
         v = list(pd.read_csv(f"{embedding_path}/{r}").iloc[:, 0].values)
-        db_collection.add(
-            embeddings=[v],
-            ids=[instance_id]
-        )
+        for db_collection in db_collection_list:
+            db_collection.add(
+                embeddings=[v],
+                ids=[instance_id]
+            )
         display_progress(idx+1, len(files))
-    print(f"DB load {db_collection.name} done!")
-    return db_collection
+    print(f"DB load path {embedding_path} done")
 
 
 def file_name(file):
--- src/main.py
+++ src/main.py
@@ -10,13 +10,19 @@ from src.db import init_db_collection
 from src.utils import img_url, alignment_url, arches_score
 
 embedding_path = os.environ.get("RCSB_EMBEDDING_PATH")
+csm_path = os.environ.get("CSM_EMBEDDING_PATH")
 assembly_path = os.environ.get("RCSB_ASSEMBLY_PATH")
+
 if not embedding_path or not os.path.isdir(embedding_path):
     raise Exception(f"Embedding path {embedding_path} is not available")
+
+if not csm_path or not os.path.isdir(csm_path):
+    raise Exception(f"Embedding path {csm_path} is not available")
+
 if not assembly_path or not os.path.isdir(assembly_path):
     raise Exception(f"Embedding path {assembly_path} is not available")
 
-chain_collection, assembly_collection = init_db_collection(embedding_path, assembly_path)
+chain_collection, csm_collection, assembly_collection = init_db_collection(embedding_path, csm_path, assembly_path)
 app = FastAPI()
 templates = Jinja2Templates(directory="src/templates")
 
@@ -24,7 +30,7 @@ _ef_search = 10000
 
 
 @app.get("/embedding_search/{rcsb_id}", response_class=HTMLResponse)
-async def search_chain(request: Request, rcsb_id: str, granularity: str = "chain", n_results: int = 100):
+async def search_chain(request: Request, rcsb_id: str, granularity: str = "chain", n_results: int = 100, include_csm: bool = False):
     if not os.path.isfile(f"{embedding_path}/{rcsb_id}.csv") and not os.path.isfile(f"{assembly_path}/{rcsb_id}.csv"):
         random_id = ".".join(random.choice(os.listdir(embedding_path)).split(".")[0:2])
         context = {"rcsb_id": rcsb_id, "search_id": random_id, "request": request}
@@ -36,7 +42,7 @@ async def search_chain(request: Request, rcsb_id: str, granularity: str = "chain
         if os.path.isfile(f"{embedding_path}/{rcsb_id}.csv") \
         else list(pd.read_csv(f"{assembly_path}/{rcsb_id}.csv").iloc[:, 0].values)
 
-    result = (chain_collection if granularity != "assembly" else assembly_collection).query(
+    result = (assembly_collection if granularity == "assembly" else (csm_collection if include_csm else chain_collection)).query(
         query_embeddings=[rcsb_embedding],
         n_results=n_results if n_results > _ef_search else _ef_search
     )
@@ -52,9 +58,18 @@ async def search_chain(request: Request, rcsb_id: str, granularity: str = "chain
     if n_results < _ef_search:
         results = results[0:n_results]
 
-    context = {"search_id": rcsb_id, "results": results, "request": request}
+    context = {
+        "search_id": rcsb_id,
+        "results": results,
+        "request": request,
+        "granularity": granularity,
+        "n_results": n_results,
+        "include_csm": include_csm
+    }
+
     return templates.TemplateResponse(
-        name="search.html.jinja", context=context
+        name="search.html.jinja",
+        context=context
     )
 
 
