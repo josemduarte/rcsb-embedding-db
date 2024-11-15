533680ed3fc1f42e6543736c94fa73a986b83e6b
bioinsilico <jsegurmor@gmail.com>
2024-11-15 11:27:13 -0800

    recursion code bug fixed

--- src/load_embeddings.py
+++ src/load_embeddings.py
@@ -8,23 +8,22 @@ import multiprocessing as mp
 af_embedding_folder = "/mnt/vdc1/computed-models/embeddings"
 collection_name = 'af_embeddings'
 dim = 1280
+embedding_db = EmbeddingDB(
+    collection_name,
+    dim
+)
 
 
-def insert_file(x):
-    embedding_db, file = x
+def insert_file(file):
     embedding_db.insert_df(pd.read_pickle(file))
 
 
 def main():
-    embedding_db = EmbeddingDB(
-        collection_name,
-        dim
-    )
     num_processes = mp.cpu_count()
     with mp.Pool(processes=num_processes) as pool:
         for _ in pool.imap_unordered(
                 insert_file,
-                [(embedding_db, f'{af_embedding_folder}/{df}') for df in os.listdir(af_embedding_folder)]
+                [f'{af_embedding_folder}/{df}' for df in os.listdir(af_embedding_folder)]
         ):
             pass
     embedding_db.index_collection()
